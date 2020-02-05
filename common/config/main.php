916249d344a0d2d3e36d7d20c59f07425020e223
amun <a.mun@sdutionx.ru>
2020-02-05 18:46:26 +0300

    history

--- common/config/main.php
+++ common/config/main.php
@@ -11,6 +11,29 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db_reg.ru' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=31.31.196.23;dbname=u0128646_default',
+            'username' => 'u0128646_default',
+            'password' => '9Bh!4CZr',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'ssl://smtp.yandex.ru',
+                'username' => 'arth.moon@ya.ru',
+                'password' => 'astana2018',
+                'port' => '465',
+                'encryption' => 'SSL',
+            ],
+        ],
     ],
     'modules' => [
         'gridview' =>  [
